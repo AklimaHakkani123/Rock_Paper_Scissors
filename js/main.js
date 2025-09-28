94fbb119c52af87abe6270ef7c9fb89039413d46
Aklima Hakkani <186603940+AklimaHakkani123@users.noreply.github.com>
2025-09-28 17:52:37 +0600

    problem solving number 3

--- js/main.js
+++ js/main.js
@@ -14,8 +14,43 @@
 // console.log(getComputerChoice());
 
 // problem human Choice
-function getHumanChoice() {
-  const choice = prompt("choice");
-  return choice.toLowerCase();
+// function getHumanChoice() {
+//   const choice = prompt("choice");
+//   return choice.toLowerCase();
+// }
+// console.log(getHumanChoice());
+
+//players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+//logic to play a single round
+// function playRound(humanChoice, computerChoice) {
+//   computerChoice = computerChoice.toLowerCase();
+//   humanChoice = humanChoice.toLowerCase();
+//   if (humanChoice === computerChoice) {
+//     console.log("it's a tie, both choice " + humanChoice);
+//     return "tie";
+//   } else if (
+//     (humanChoice === "rock" && computerChoice === "scissors") ||
+//     (humanChoice === "paper" && computerChoice === "rock") ||
+//     (humanChoice === "scissors" && computerChoice === "paper")
+//   ) {
+//     console.log("You win!  " + humanChoice + "  beats  " + computerChoice);
+
+//     humanScore++;
+//     return "human";
+//   } else {
+//     console.log("You lose!" + computerChoice + "beats" + humanChoice);
+//     computerScore++;
+//     return "computer";
+//   }
+// }
+// Write the logic to play the entire game
+
+function playGame() {
+  humanScore = 0;
+  computerScore = 0;
+  for (i = 1; i <= 5; i++) {}
 }
-console.log(getHumanChoice());
+console.log("hima");
